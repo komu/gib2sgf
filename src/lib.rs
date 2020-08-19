46a62c91e8f55ddeaf3c9d1b5d36db26b41029f7
Juha Komulainen <juha.komulainen@evident.fi>
2020-08-19 09:02:38 +0300

    Use wee_alloc as allocator

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ use wasm_bindgen::prelude::*;
 
 pub use crate::gib2sgf::gib_to_sgf;
 
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 pub fn convert_gib_to_sgf(str: &str) -> Option<String> {
     gib_to_sgf(str).ok()
