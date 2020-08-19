4cbf8a771de7622dfea9a7faac4c57c659ac0c66
Juha Komulainen <juha.komulainen@evident.fi>
2020-08-19 08:54:33 +0300

    Support building to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,11 @@ mod gib;
 mod go;
 mod sgf;
 mod gib2sgf;
+use wasm_bindgen::prelude::*;
 
 pub use crate::gib2sgf::gib_to_sgf;
+
+#[wasm_bindgen]
+pub fn convert_gib_to_sgf(str: &str) -> Option<String> {
+    gib_to_sgf(str).ok()
+}
