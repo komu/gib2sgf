3a95a4533dfe0bded9e9ecb33c88b60b8a7529f6
Juha Komulainen <juha.komulainen@evident.fi>
2020-08-20 21:59:05 +0300

    Add mode for testing that files stay identical

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod files;
+
 use std::fs;
 use std::env;
 use std::path::Path;
@@ -11,11 +12,25 @@ fn main() {
 
     let files = collect_files(Path::new(args.get(1).unwrap_or(&String::from(".")))).unwrap();
     let mut converted = false;
+    let test = env::var("TEST").is_ok();
 
     for gib_path in files {
         let sgf_path = normalize_path(&gib_path).with_extension("sgf");
 
-        if !sgf_path.exists() {
+        if test {
+            let gib_contents = fs::read_to_string(&gib_path).unwrap();
+            let sgf = gib_to_sgf(&gib_contents).unwrap();
+            if let Ok(existing_sgf) = fs::read_to_string(&sgf_path) {
+                let old = remove_app_version_from_sgf(&existing_sgf);
+                let new = remove_app_version_from_sgf(&sgf);
+
+                if old != new {
+                    println!("Results differ on {:?}\n   old: {}\n   new: {}", gib_path, old, new);
+                }
+            }
+
+            // TODO: check that output is equal to previously generated SGF file
+        } else if !sgf_path.exists() {
             println!("Convert {:?} -> {:?}", gib_path, sgf_path);
 
             // Convert file
@@ -31,7 +46,17 @@ fn main() {
         }
     }
 
-    if !converted {
+    if !test && !converted {
         println!("no unconverted files")
     }
+}
+
+fn remove_app_version_from_sgf(str: &str) -> String {
+    if let Some(start) = str.find("AP[gib2sgf:") {
+        let suffix = &str[start..];
+        if let Some(end) = suffix.find("]") {
+            return format!("{}{}", &str[..start], &suffix[end + 1..]);
+        }
+    }
+    return str.to_string();
 }
